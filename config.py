bce687d0a18fea032c49ae50c96381b1e6c38638
usernamex007 <sonalixsachin@gmail.com>
2025-02-10 15:58:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Replace these with your actual API details
 API_ID = "28795512"  # Replace with your API ID Get From my.telegram.org
 API_HASH = "c17e4eb6d994c9892b8a8b6bfea4042a"  # Replace with your API Hash Get From my.telegram.org
-BOT_TOKEN = "7767480564:AAG2k3u9XhDJ9WEC-0Qgm0AQQz8y1ZFfaWw"  # Replace with your Bot Token Get From @BotFather 
+BOT_TOKEN = "7782975743:AAHLO_wbYCBLXwPke-Dt1afPmu8LwThKq7g"  # Replace with your Bot Token Get From @BotFather 
